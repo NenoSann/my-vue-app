a9917a4dc9855b1335fd365d164296ae84ec4891
NenoSan <lianghengCN@gmail.com>
2023-11-17 11:43:42 +0800

    integrate: add tailwind and daisyui

---
+++ src/component/model.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class=" bg-black">
+        <textarea name="text" id="text" cols="30" rows="10" v-model="context"></textarea>
+        <p class="text-white">{{ context }}</p>
+        <div class="daisy-btn">BUTTON</div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+const context = ref<string>();
+</script>
+
+<style scoped></style>
