a038ad7614210ca4e118d5d58a46c386c6bb9b24
NenoSan <lianghengCN@gmail.com>
2023-11-27 10:19:46 +0800

    add typescript and config it

--- src/main.ts
+++ src/main.ts
@@ -1,54 +1,59 @@
-"use strict";
-exports.__esModule = true;
-var electron_1 = require("electron");
-var path = require("path");
+import { app, BrowserWindow } from 'electron';
+import * as path from 'path';
+
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
-    electron_1.app.quit();
+    app.quit();
 }
-var createWindow = function () {
+
+const createWindow = () => {
     // Create the browser window.
-    var mainWindow = new electron_1.BrowserWindow({
+    const mainWindow = new BrowserWindow({
         width: 800,
         height: 600,
         minHeight: 600,
         minWidth: 800,
         autoHideMenuBar: true,
         webPreferences: {
-            preload: path.join(__dirname, 'preload.js')
-        }
+            preload: path.join(__dirname, 'preload.js'),
+        },
     });
+
     // and load the index.html of the app.
     //@ts-ignore
-    if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
+    if (MAIN_WINDOW_VITE_DEV_SERVER_URL as any) {
         //@ts-ignore
-        mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
-    }
-    else {
+        mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL as any);
+    } else {
         //@ts-ignore
-        mainWindow.loadFile(path.join(__dirname, "../renderer/".concat(MAIN_WINDOW_VITE_NAME, "/index.html")));
+        mainWindow.loadFile(path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`));
     }
+
     // Open the DevTools.
     // mainWindow.webContents.openDevTools();
 };
+
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-electron_1.app.on('ready', createWindow);
+app.on('ready', createWindow);
+
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-electron_1.app.on('window-all-closed', function () {
+app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
-        electron_1.app.quit();
+        app.quit();
     }
 });
-electron_1.app.on('activate', function () {
+
+app.on('activate', () => {
     // On OS X it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
-    if (electron_1.BrowserWindow.getAllWindows().length === 0) {
+    if (BrowserWindow.getAllWindows().length === 0) {
         createWindow();
     }
 });
+
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
--- src/preload.js
+++ src/preload.js
@@ -1,10 +1,15 @@
+"use strict";
+exports.__esModule = true;
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
-const { contextBridge } = require('electron');
-
-contextBridge.exposeInMainWorld('versions', {
-    node: () => process.versions.node,
-    chrome: () => process.versions.chrome,
-    electron: () => process.versions.electron,
+var electron_1 = require("electron");
+electron_1.contextBridge.exposeInMainWorld('versions', {
+    node: function () { return process.versions.node; },
+    chrome: function () { return process.versions.chrome; },
+    electron: function () { return process.versions.electron; },
     author: 'NenoSan'
-})
+});
+electron_1.contextBridge.exposeInMainWorld('storeUserInfo', {
+    save: function () {
+    }
+});
