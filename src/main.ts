3bc2231a35d065a4680cb9ec81cd96efa15e1d21
LiangHeng <lianghengcn@gmail.com>
2024-03-27 00:06:32 +0800

    feat: add system tray for app

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { app, BrowserWindow, ipcMain, nativeImage } from 'electron';
+import { app, BrowserWindow, ipcMain, nativeImage, Menu, Tray, Screen, screen } from 'electron';
 import * as path from 'path';
 import { Socketio } from './Node_Util/index.ts';
 import { WorkerController } from './Node_Util/WorkerController.ts';
@@ -9,10 +9,10 @@ if (require('electron-squirrel-startup')) {
     app.quit();
 }
 let mainWindow: BrowserWindow | undefined;
-
+const icon = nativeImage.createFromPath('D:\\Web\\ElectronVueApp\\assets\\icon.png');
+let tray: Tray;
 const createWindow = () => {
     // Create the browser window.
-    const icon = nativeImage.createFromPath('../assets/icon.png');
     mainWindow = new BrowserWindow({
         width: 800,
         height: 600,
@@ -23,8 +23,18 @@ const createWindow = () => {
             preload: path.join(__dirname, 'preload.js'),
             nodeIntegration: true
         },
-        icon: icon
+        icon: icon,
+        titleBarOverlay: true,
     });
+    mainWindow.on('close', (event) => {
+        //@ts-ignore
+        if (app.quitting as unknown as any) {
+            mainWindow = undefined;
+        } else {
+            event.preventDefault();
+            mainWindow?.hide();
+        }
+    })
 
     // and load the index.html of the app.
     //@ts-ignore
@@ -37,23 +47,46 @@ const createWindow = () => {
     }
 
     // Open the DevTools.
-    mainWindow.webContents.openDevTools();
+    // mainWindow.webContents.openDevTools();
 };
+
+const createTray = () => {
+    tray = new Tray(icon);
+    const contextMenu = Menu.buildFromTemplate([
+        { label: '好友', type: 'normal' },
+        { label: '打开面板', type: 'normal', click: () => mainWindow?.show() },
+        {
+            label: '退出', type: 'normal', click: () => app.quit()
+        }
+    ]);
+    tray.on('right-click', () => {
+        tray.popUpContextMenu(contextMenu);
+    });
+}
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 
-app.on('ready', createWindow);
+app.on('ready', () => {
+    createWindow();
+    createTray();
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
+    // if (process.platform !== 'darwin') {
+    //     app.quit();
+    // }
 });
+
+app.on('before-quit', () => {
+    //@ts-ignore
+    // the 'quitting' is self defined property
+    app.quitting = true;
+})
 
 app.on('activate', () => {
     // On OS X it's common to re-create a window in the app when the
@@ -62,6 +95,7 @@ app.on('activate', () => {
         createWindow();
     }
 });
+
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
--- vite.main.config.mjs
+++ vite.main.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
-
 // https://vitejs.dev/config
 export default defineConfig({
+  assetsInclude: ['./assets/*.jpg'],
   resolve: {
     // Some libs that can run in both Web and Node.js, such as `axios`, we need to tell Vite to build them in Node.js.
     browserField: false,
