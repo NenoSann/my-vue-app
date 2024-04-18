ac4788f5b391c4104d7ac306ae3e2018b74e2ec0
LiangHeng <lianghengcn@gmail.com>
2024-04-19 04:27:31 +0800

    feat: add BufferControlller, ShellController and Dialog Controller to handle file operation feat: add api to handle image selectron

--- src/Electron/index.ts
+++ src/Electron/index.ts
@@ -1,3 +1,12 @@
 import { NotificationController } from "./NotificationController";
 import { WindowsController } from "./WindowsController";
-export { NotificationController, WindowsController }
+import { DialogController } from "./DialogController";
+import { BufferController } from './BufferController';
+import { ShellController } from './ShellController';
+export {
+    NotificationController,
+    WindowsController,
+    DialogController,
+    BufferController,
+    ShellController
+}
